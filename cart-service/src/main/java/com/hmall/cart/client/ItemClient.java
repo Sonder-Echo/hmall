0a7ade81c3e8ddb18315b5c5951dd024cd1c395b
sonder <382547989@qq.com>
2025-06-23 19:31:03 +0800

    通过OpenFeign远程服务调用，优化RestTemplate和DiscoveryClient编写的大量重复代码

---
+++ cart-service/src/main/java/com/hmall/cart/client/ItemClient.java
@@ -0,0 +1,24 @@
+package com.hmall.cart.client;
+
+import com.hmall.cart.domain.dto.ItemDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.List;
+
+//标注是一个Feign客户端，然后指定了微服务名称，这样可以获取该微服务的实例列表:
+//并基于负载均衡选择一个服务实例
+@FeignClient("item-service")
+public interface ItemClient {
+
+    //在接口内：编写要远程调用的方法，这些方法都可以参考自服务提供者（item-service）对应的接口
+
+
+    //根据商品id集合获取商品dto列表
+    @GetMapping("/items")
+    public List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+}
