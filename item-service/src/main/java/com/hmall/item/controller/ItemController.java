80adb4f8eac04497b9d27c319b2566bc6215d54f
sonder <382547989@qq.com>
2025-06-24 16:47:25 +0800

    全局过滤器

--- item-service/src/main/java/com/hmall/item/controller/ItemController.java
+++ item-service/src/main/java/com/hmall/item/controller/ItemController.java
@@ -28,7 +28,8 @@ public class ItemController {
 
     @ApiOperation("分页查询商品")
     @GetMapping("/page")
-    public PageDTO<ItemDTO> queryItemByPage(PageQuery query) {
+    public PageDTO<ItemDTO> queryItemByPage(PageQuery query, @RequestHeader(value = "testStr", required = false)String testStr) {
+        System.out.println("从请求头中获取到的testStr = "+testStr);
         // 1.分页查询
         Page<Item> result = itemService.page(query.toMpPage("update_time", false));
         // 2.封装并返回
